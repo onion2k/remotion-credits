cb9e406af67a75028fda450ea03669fe42d93ea4
Chris Neale <chris.neale@lush.co.uk>
2022-06-28 13:51:53 +0100

    Initialized based on remotion-confetti

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { useCurrentFrame, useVideoConfig } from 'remotion';
+
+export const Credits = () => {
+
+    const frame = useCurrentFrame();
+    const video = useVideoConfig();
+
+    return null;
+};
+
+export default Credits;
